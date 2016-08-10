1d09fdd7a586f74dc1204e41380c68eca2533d99
ChalkPE <chalk@chalk.pe>
2016-08-10 12:06:09 +0900

    Added MongoDB integration

--- convert.js
+++ convert.js
@@ -1,21 +1,45 @@
 var glob = require('glob');
 var jsonfile = require('jsonfile');
 var lineReader = require('line-reader');
+var MongoClient = require('mongodb').MongoClient;
 
 jsonfile.spaces = 4;
-glob('csv/*.csv', (err, csvFiles) => csvFiles.forEach(csvFile => {
-    let days = {};
-    lineReader.eachLine(csvFile, { encoding: 'utf8' }, (line, last) => {
-        let data = line.split(';').map(v => v.substring(1, v.length - 1));
-        let [day, word, meaning, level] = [parseInt(data[1], 10), data[2], data[3], parseInt(data[4], 10)];
 
-        days[day] = days[day] || { _id: day, words: [] };
-        days[day].words.push({ word, meaning, level });
+MongoClient.connect('mongodb://localhost:27017/voca', (err, db) => {
+    if(err) return console.log(err);
 
-        if(last){
-            let json = { collections: [] };
-            for(let i of Object.keys(days)) json.collections.push(days[i]);
-            jsonfile.writeFileSync(csvFile.replace(/csv/, "json"), json);
-        }
+    glob('csv/*.csv', (err, csvFiles) => {
+        if(err) return console.log(err);
+        console.log("glob", csvFiles);
+
+        csvFiles.forEach(csvFile => {
+            let days = {};
+            let colName = 'days_' + csvFile.replace(/(csv|\.|\/)/g, '');
+
+            console.log(colName);
+
+            db.createCollection(colName, (err, col) => {
+                if(err) return console.log(err);
+                console.log(colName, "createCollection");
+
+                lineReader.eachLine(csvFile, { encoding: 'utf8' }, (line, last) => {
+                    let data = line.split(';').map(v => v.substring(1, v.length - 1));
+                    let [day, word, meaning, level] = [parseInt(data[1], 10), data[2], data[3], parseInt(data[4], 10)];
+
+                    days[day] = days[day] || { _id: day, words: [] };
+                    days[day].words.push({ word, meaning, level });
+
+                    if(last) col.deleteMany({}, (err, result) => {
+                        if(err) return console.log(err);
+                        console.log(colName, "deleteMany");
+
+                        col.insert(Object.keys(days).map(k => days[k]), (err, result) => {
+                            if(err) return console.log(err);
+                            console.log(colName, "insert", result.insertedCount);
+                        });
+                    });
+                });
+            });
+        });
     });
-}));
+});
