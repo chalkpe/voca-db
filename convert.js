0bc0823f3a11406b4b342fb7305e445cec3f6e1f
ChalkPE <chalk@chalk.pe>
2016-08-24 20:51:11 +0900

    Added embedded image importing

--- convert.js
+++ convert.js
@@ -1,39 +1,57 @@
-var [colName, glob, lineReader] = ['days', require('glob'), require('line-reader')];
+var [fs, glob, lineReader, names] = [require('fs'), require('glob'), require('line-reader'), require('./names.json')];
 
 let log = (...args) => console.log(new Date().toISOString(), ...args);
 let err = (error) => console.error(new Date().toISOString(), error);
 
 require('mongodb').MongoClient.connect('mongodb://localhost:27017/voca', (err, db) => {
     if(err) return console.error(err);
-    log("MongoClient.connect");
+    log(`MongoClient.connect`);
 
-    db.createCollection(colName, (err, col) => {
+    db.createCollection('books', (err, books) => {
         if(err) return console.error(err);
-        log("db.createCollection", colName);
+        log(`db.createCollection`, `books`);
 
-        col.deleteMany({}, (err, result) => {
+        books.deleteMany({}, (err, result) => {
             if(err) return console.error(err);
-            log("col.deleteMany", colName);
+            log(`books.deleteMany`);
 
-            glob('csv/*.csv', (err, csvFiles) => {
+            db.createCollection('days', (err, days) => {
                 if(err) return console.error(err);
-                log("glob", csvFiles);
+                log(`db.createCollection`, `days`);
 
-                csvFiles.forEach(csvFile => {
-                    let dayMap = {};
-                    let bookName = csvFile.replace(/(csv|\.|\/)/g, '');
-                    log("csvFiles.forEach", colName, bookName);
+                days.deleteMany({}, (err, result) => {
+                    if(err) return console.error(err);
+                    log(`days.deleteMany`);
 
-                    lineReader.eachLine(csvFile, { encoding: 'utf8' }, (line, last) => {
-                        let data = line.split(';').map(v => v.substring(1, v.length - 1));
-                        let [day, word, meaning, level] = [parseInt(data[1], 10), data[2], data[3], parseInt(data[4], 10)];
+                    glob('csv/*.csv', (err, files) => {
+                        if(err) return console.error(err);
+                        log(`glob`, files.join());
 
-                        dayMap[day] = dayMap[day] || { book: bookName, day, words: [] };
-                        dayMap[day].words.push({ word, meaning, level });
+                        files.forEach(file => {
+                            log(`files.forEach`, file);
+                            let dayMap = {}, bookId = file.replace(/(csv|\.|\/)/g, '');
 
-                        if(last) col.insert(Object.keys(dayMap).map(k => dayMap[k]), (err, result) => {
-                            if(err) return console.error(err);
-                            log("col.insert", colName, bookName, result.insertedCount);
+                            lineReader.eachLine(file, { encoding: 'utf8' }, (line, last) => {
+                                let data = line.split(';').map(v => v.substring(1, v.length - 1));
+                                let [day, word, meaning, level] = [parseInt(data[1], 10), data[2], data[3], parseInt(data[4], 10)];
+
+                                (dayMap[day] = dayMap[day] || { book: bookId, day, words: [] }).words.push({ word, meaning, level });
+
+                                if(last) days.insert(Object.keys(dayMap).map(k => dayMap[k]), (err, result) => {
+                                    if(err) return console.error(err);
+                                    log(`days.insert`, bookId, result.insertedCount);
+
+                                    fs.readFile(`img/${bookId}.jpg`, (err, data) => {
+                                        if(err) return console.error(err);
+                                        log(`fs.readFile`, bookId);
+
+                                        books.insert({ id: bookId, name: names[bookId], image: 'data:image/jpeg;base64,' + new Buffer(data).toString('base64') }, (err, result) => {
+                                            if(err) return console.error(err);
+                                            log(`books.insert`, bookId, result.insertedCount);
+                                        });
+                                    });
+                                });
+                            });
                         });
                     });
                 });
