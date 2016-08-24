abfaf6a0b9688a24783542c7068f11dcee97b31b
ChalkPE <chalk@chalk.pe>
2016-08-24 21:05:02 +0900

    Fixed a log error

--- convert.js
+++ convert.js
@@ -4,27 +4,27 @@ let log = (...args) => console.log(new Date().toISOString(), ...args);
 let err = (error) => console.error(new Date().toISOString(), error);
 
 require('mongodb').MongoClient.connect('mongodb://localhost:27017/voca', (err, db) => {
-    if(err) return console.error(err);
+    if(err) return error(err);
     log(`MongoClient.connect`);
 
     db.createCollection('books', (err, books) => {
-        if(err) return console.error(err);
+        if(err) return error(err);
         log(`db.createCollection`, `books`);
 
         books.deleteMany({}, (err, result) => {
-            if(err) return console.error(err);
+            if(err) return error(err);
             log(`books.deleteMany`);
 
             db.createCollection('days', (err, days) => {
-                if(err) return console.error(err);
+                if(err) return error(err);
                 log(`db.createCollection`, `days`);
 
                 days.deleteMany({}, (err, result) => {
-                    if(err) return console.error(err);
+                    if(err) return error(err);
                     log(`days.deleteMany`);
 
                     glob('csv/*.csv', (err, files) => {
-                        if(err) return console.error(err);
+                        if(err) return error(err);
                         log(`glob`, files.join());
 
                         files.forEach(file => {
@@ -38,15 +38,15 @@ require('mongodb').MongoClient.connect('mongodb://localhost:27017/voca', (err, d
                                 (dayMap[day] = dayMap[day] || { book: bookId, day, words: [] }).words.push({ word, meaning, level });
 
                                 if(last) days.insert(Object.keys(dayMap).map(k => dayMap[k]), (err, result) => {
-                                    if(err) return console.error(err);
+                                    if(err) return error(err);
                                     log(`days.insert`, bookId, result.insertedCount);
 
                                     fs.readFile(`img/${bookId}.jpg`, (err, data) => {
-                                        if(err) return console.error(err);
+                                        if(err) return error(err);
                                         log(`fs.readFile`, bookId);
 
                                         books.insert({ id: bookId, name: names[bookId], image: 'data:image/jpeg;base64,' + new Buffer(data).toString('base64') }, (err, result) => {
-                                            if(err) return console.error(err);
+                                            if(err) return error(err);
                                             log(`books.insert`, bookId, result.insertedCount);
                                         });
                                     });
